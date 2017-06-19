a3af4d5a0b67dfb2476f5def07515b91a9117c8b
Stephen <stephenbylo@gmail.com>
2017-06-19 15:01:06 +0800

    mod

--- s2i-simple-builder/myapp/build.sh
+++ s2i-simple-builder/myapp/build.sh
@@ -6,7 +6,13 @@ oc new-app myapp
 
 oc get pod
 
-oc logs <pod>
+#oc logs <pod>
+
+# Alternative way ...
+oc new-app s2i-simple-builder~https://github.com/sjbylo/ocp-topics \
+   --context-dir s2i-simple-builder/myapp --name myapp2
+
+# or...
 
 # oc new-build --binary=true -i s2i-simple-builder --name myapp 
 # oc start-build myapp --from-dir=. 
