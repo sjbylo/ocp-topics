f61062da657cfff6f87eb931f689b4f910bc88e8
Stephen <stephenbylo@gmail.com>
2017-06-19 12:44:41 +0800

    mod

--- s2i-builder-simple/builder/build.sh
+++ s2i-builder-simple/builder/build.sh
@@ -1,6 +1,7 @@
 oc import-image openshift/base-centos7 --confirm
 
-oc new-build . --name s2i-simple-builder
+#oc new-build . --name s2i-simple-builder 
+oc new-build --name s2i-simple-builder --binary=true
 
 oc start-build s2i-simple-builder --from-dir=.
 
