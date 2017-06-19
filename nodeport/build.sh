e281b10dea9469126bf6f8a7f92a049e70b76232
Stephen <stephenbylo@gmail.com>
2017-06-19 19:39:08 +0800

    mod

--- nodeport/build.sh
+++ nodeport/build.sh
@@ -1,9 +1,5 @@
-# Build with docker 
-
-docker login docker.io -u sjbylo
-docker build . -t sjbylo/netcat-echo
-docker run -it --rm sjbylo/netcat-echo -p 2000:2000
-docker push sjbylo/netcat-echo
+# Simply pull the netcat-echo application from docker hub and launch it on OpenShift.
+oc new-app sjbylo/netcat-echo 
 
 # Docker build on OpenShift
 
@@ -11,4 +7,15 @@ oc new-build --binary --name netcat-echo
 oc start-build netcat-echo --from-dir=.
 oc logs bc/netcat-echo -f
 oc new-app netcat-echo
+
+# Build with plain docker 
+
+docker build . -t sjbylo/netcat-echo
+docker run -d --rm -p 2000:2000 --name echo sjbylo/netcat-echo 
+docker kill echo 
+
+# Push to docker hub
+
+docker login docker.io -u sjbylo
+docker push sjbylo/netcat-echo
 
